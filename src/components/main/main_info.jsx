246ee978834d0f50ea95534c61a000b25a6ad980
D-Diyazzz <diyazzz.dev@gmail.com>
2025-03-08 22:22:24 +0500

    add all components for main page

--- src/components/main/main_info.jsx
+++ src/components/main/main_info.jsx
@@ -1,7 +1,7 @@
 import main_2_png from "../../style/images/main-2.png"
 
 
-export default function MainInfo(){
+export default function MainInfo({ collections, handleNext, handlePrev }){
 	return (
 		<>
 			<div className="main-info-backgroud-elem">
@@ -28,7 +28,6 @@ export default function MainInfo(){
 					<p className="main-info-back-elem-3-3">1917</p>
 				</div>
 
-
 					<div className="main-info-main">
 						<div className="main-info-main-title">
 							E-Doc Complex
@@ -39,13 +38,19 @@ export default function MainInfo(){
 						<a href="#" className="main-link-collection">Все коллекции</a>
 						
 						<div className="main-while-button">
-							<button><svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<circle cx="25" cy="25" r="26" transform="matrix(-1 0 0 1 52 2)" stroke="#B7C1EF" stroke-width="2"/>
-		<path d="M37 26C37.5523 26 38 26.4477 38 27C38 27.5523 37.5523 28 37 28V26ZM17.2929 27.7071C16.9024 27.3166 16.9024 26.6834 17.2929 26.2929L23.6569 19.9289C24.0474 19.5384 24.6805 19.5384 25.0711 19.9289C25.4616 20.3195 25.4616 20.9526 25.0711 21.3431L19.4142 27L25.0711 32.6569C25.4616 33.0474 25.4616 33.6805 25.0711 34.0711C24.6805 34.4616 24.0474 34.4616 23.6569 34.0711L17.2929 27.7071ZM37 28L18 28V26L37 26V28Z" fill="#B7C1EF"/></svg></button>
-							
-							<button><svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<circle cx="27" cy="27" r="26" stroke="#B7C1EF" stroke-width="2"/>
-		<path d="M17 26C16.4477 26 16 26.4477 16 27C16 27.5523 16.4477 28 17 28V26ZM36.7071 27.7071C37.0976 27.3166 37.0976 26.6834 36.7071 26.2929L30.3431 19.9289C29.9526 19.5384 29.3195 19.5384 28.9289 19.9289C28.5384 20.3195 28.5384 20.9526 28.9289 21.3431L34.5858 27L28.9289 32.6569C28.5384 33.0474 28.5384 33.6805 28.9289 34.0711C29.3195 34.4616 29.9526 34.4616 30.3431 34.0711L36.7071 27.7071ZM17 28L36 28V26L17 26V28Z" fill="#B7C1EF"/></svg></button>
+
+							<button onClick={handlePrev}>
+							  <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg"> <circle cx="25" cy="25" r="26" transform="matrix(-1 0 0 1 52 2)" stroke="#B7C1EF" strokeWidth="2" />
+								<path d="M37 26C37.5523 26 38 26.4477 38 27C38 27.5523 37.5523 28 37 28V26ZM17.2929 27.7071C16.9024 27.3166 16.9024 26.6834 17.2929 26.2929L23.6569 19.9289C24.0474 19.5384 24.6805 19.5384 25.0711 19.9289C25.4616 20.3195 25.4616 20.9526 25.0711 21.3431L19.4142 27L25.0711 32.6569C25.4616 33.0474 25.4616 33.6805 25.0711 34.0711C24.6805 34.4616 24.0474 34.4616 23.6569 34.0711L17.2929 27.7071ZM37 28L18 28V26L37 26V28Z" fill="#B7C1EF"/>
+							  </svg>
+							</button>
+
+							<button onClick={handleNext}>
+							  <svg width="54" height="54" viewBox="0 0 54 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+								<circle cx="27" cy="27" r="26" stroke="#B7C1EF" strokeWidth="2" />
+								<path d="M17 26C16.4477 26 16 26.4477 16 27C16 27.5523 16.4477 28 17 28V26ZM36.7071 27.7071C37.0976 27.3166 37.0976 26.6834 36.7071 26.2929L30.3431 19.9289C29.9526 19.5384 29.3195 19.5384 28.9289 19.9289C28.5384 20.3195 28.5384 20.9526 28.9289 21.3431L34.5858 27L28.9289 32.6569C28.5384 33.0474 28.5384 33.6805 28.9289 34.0711C29.3195 34.4616 29.9526 34.4616 30.3431 34.0711L36.7071 27.7071ZM17 28L36 28V26L17 26V28Z" fill="#B7C1EF"/>
+							  </svg>
+							</button>
 						</div>
 				</div>
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,52 @@
+import { useEffect, useState } from "react"
+import api from "../api"
 import Header from "../components/header"
 import MainInfo from "../components/main/main_info"
+import MainFirstColl from "../components/main/main_last_coll"
 
 import "../style/partials/home.css"
+import MainLastComponent from "../components/main/main_last_component"
 
 
 export default function Main(){
+
+	const componentsList = [MainInfo, MainFirstColl, MainLastComponent];
+	const [collections, setCollections] = useState([]);
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	useEffect(() => {
+		const getCollections = async () => {
+			try {
+				const response = await api.get("/collection");
+				setCollections(response.data);
+				console.log("Response:", response);
+		  	} catch (error) {
+				console.error("Ошибка при получении коллекций:", error);
+		  	}
+		};
+		console.log("useEffect");
+		getCollections();
+	}, []);
+
+	const handleNext = () => {
+		setCurrentIndex((prevIndex) => (prevIndex + 1) % componentsList.length);
+	};
+
+	const handlePrev = () => {
+		setCurrentIndex((prevIndex) => (prevIndex - 1 + componentsList.length) % componentsList.length);
+	};
+
+	const CurrentComponent = componentsList[currentIndex];
+
 	return (
 		<>
-			<Header active={"main"}/>
-			<MainInfo/>
+			<Header active="main" />
+		  	{/* Передаём коллекции и функции переключения в дочерний компонент */}
+		  	<CurrentComponent 
+				collections={collections} 
+				handleNext={handleNext} 
+				handlePrev={handlePrev}
+		  	/>
 		</>
-	)
+	);
 }
